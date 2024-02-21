f04f62bb3dbc548155d33c8cefcc121f1fed9c53
Lakshay Dhingra <lakshaydhingra32969@gmail.com>
2024-02-22 02:08:00 +0530

    04 Basic Dashboard Completed

--- Installing_Packages.R
+++ Installing_Packages.R
@@ -1,7 +1,8 @@
 # Installing the required Packages
 reqd_packages = c('flexdashboard', 'tidyverse', 'highcharter', 'gt', 'htmltools',
-                  'viridis', 'moments', 'psych')
+                  'viridis', 'moments', 'psych', 'plotly')
 install.packages(reqd_packages)
 
 colors = read.csv("./Color_Pallete.csv")
 blue_pallete = colors[['Blue_Pallete']]
+
