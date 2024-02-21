1b5f966817d82bcbb00b05b11de2ee54c5d39a7c
Lakshay Dhingra <lakshaydhingra32969@gmail.com>
2024-02-21 21:55:08 +0530

    03 First Graph

--- Installing_Packages.R
+++ Installing_Packages.R
@@ -1,4 +1,7 @@
 # Installing the required Packages
 reqd_packages = c('flexdashboard', 'tidyverse', 'highcharter', 'gt', 'htmltools',
-                  'viridis', 'moments', 'psych', 'car')
+                  'viridis', 'moments', 'psych')
 install.packages(reqd_packages)
+
+colors = read.csv("./Color_Pallete.csv")
+blue_pallete = colors[['Blue_Pallete']]
